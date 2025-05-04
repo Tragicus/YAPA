baec67578af4978d24d7bca7d7f3860f676608a6
qvermande <quentin.vermande@ens.psl.eu>
2025-05-04 18:23:39 +0200

    state monad

--- bin/main.ml
+++ bin/main.ml
@@ -2,12 +2,12 @@ open YAPA
 
 let () =
   let lexbuf = Lexing.from_channel Stdlib.stdin in
-  try ignore (List.fold_left Commands.eval Term.Context.empty (Parser.toplevel Lexer.token lexbuf))
+  try ignore (List.fold_left (fun ctx c -> fst (Commands.eval c ctx)) Term.Context.empty (Parser.toplevel Lexer.token lexbuf))
   with | Term.TypeError (ctx, e) ->
-    (try Printer.print_type_error ~debug:false ctx e;
+    (try Printer.print_type_error ~debug:false e ctx;
     Printer.pp_ctx ctx
     with
-    | Term.TypeError _ -> Printer.print_type_error ~debug:true ctx e;
+    | Term.TypeError _ -> Printer.print_type_error ~debug:true e ctx;
       Printer.pp_ctx ctx)
     | Parser.Error ->
       let pos = Lexing.lexeme_start_p lexbuf in
